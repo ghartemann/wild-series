0f30a889eec71e549e9f0068b4a60c4ce7e7d6f9
ghartemann <g.hartemann@gmail.com>
2022-05-24 16:03:43 +0200

    oops forgot to do quest 9

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', ['website' => 'Wild Series',]);
+        $programs = $programRepository->findAll();
+
+        return $this->render('program/index.html.twig', ['programs' => $programs,]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
